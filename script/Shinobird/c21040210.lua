5d156eec7ffbf987c388507904082ba73e7943ce
Neo Yuno <82581124+NeoYuno@users.noreply.github.com>
2024-08-21 12:20:33 +0300

    Add files via upload

--- script/Shinobird/c21040210.lua
+++ script/Shinobird/c21040210.lua
@@ -1,7 +1,12 @@
 --Shinobird's Descent
 local s,id=GetID()
 function s.initial_effect(c)
-	local e1=Ritual.AddProcEqual(c,s.ritualfil,8)
+	local e1=Ritual.AddProcGreater({
+		handler=c,
+		filter=s.ritualfil,
+		stage2=s.stage2
+	})
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
     --Add back
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
@@ -15,10 +20,23 @@ function s.initial_effect(c)
 	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
 end
-s.listed_names={52900000,25415052}
+s.listed_names={52900000,25415052,id}
 s.listed_card_types={TYPE_SPIRIT}
+local Shinobird={21040206,21040207,66815913,21040208,92200612,21040209,39817919,52900000,25415052,60823690,33325951,21040210,73055622,9553721,15306543,276357}
 function s.ritualfil(c)
 	return c:IsCode(52900000,25415052) and c:IsRitualMonster()
+end
+function s.thfilter(c)
+	return c:IsCode(Shinobird) and not c:IsCode(id) and c:IsAbleToHand()
+end
+function s.stage2(mat,e,tp,eg,ep,ev,re,r,rp,tc)
+	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_GRAVE,0,nil)
+	if Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_GRAVE,0,1,nil,60823690,33325951) and #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		local sg=g:Select(tp,1,1,nil)
+		if #sg==0 then return end
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+	end
 end
 
 function s.cfilter(c,tp)
